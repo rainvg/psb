8612ac5c1e95fdc458623a25acf21b5c2d5420de
lromerio <lucio.romerio@epfl.ch>
2019-04-24 02:47:12 +0200

    Experimental setup 16

--- psb/demo/main.cpp
+++ psb/demo/main.cpp
@@ -24,6 +24,11 @@ struct settings
     {
         static constexpr uint16_t port = 1234;
     };
+
+    struct synchronizer
+    {
+        static constexpr uint16_t port = 1235;
+    };
 };
 
 // Using
@@ -33,15 +38,33 @@ using namespace drop;
 
 // Functions
 
-void rendezvous()
+void rendezvous(const int & expected)
 {
     directory directory(settings :: directory :: port);
 
+    auto listener = tcp :: listen(settings :: synchronizer :: port);
+
     while(true)
-        sleep(1_h);
+    {
+        std :: vector <connection> peers;
+        peers.reserve(expected);
+
+        for(int peer = 0; peer < expected; peer++)
+        {
+            std :: cout << "Waiting for peer " << peer << " / " << expected << std :: endl;
+            peers.push_back(listener.acceptsync());
+        }
+
+        std :: cout << "All peers connected." << std :: endl;
+
+        for(const auto & connection : peers)
+            connection.sendsync(true);
+
+        sleep(10_s);
+    }
 }
 
-void peer(const int & id, const class address :: ip & directory, const interval & setuptime, const int & sources, const int & broadcasts, const interval & period, const int & batchsize, const interval & timeout)
+void peer(const int & id, const class address :: ip & directory, const int & sources, const int & broadcasts, const interval & period, const int & batchsize, const interval & timeout)
 {
     broadcast <timestamp> :: configuration :: sponge :: capacity = batchsize;
     broadcast <timestamp> :: configuration :: sponge :: timeout = timeout;
@@ -50,7 +73,9 @@ void peer(const int & id, const class address :: ip & directory, const interval 
 
     auto sampler = directory :: sample <channels> ({directory, settings :: directory :: port});
 
-    sleep(setuptime);
+    tcp :: connectsync({directory, settings :: synchronizer :: port}).receivesync <bool> ();
+
+    sleep(20_s);
 
     std :: cout << "Starting broadcast." << std :: endl;
 
@@ -98,7 +123,9 @@ void peer(const int & id, const class address :: ip & directory, const interval 
     else
         sleep(period * broadcasts);
 
-    sleep(5_m);
+    tcp :: connectsync({directory, settings :: synchronizer :: port}).receivesync <bool> ();
+
+    sleep(20_s);
 }
 
 int main(int argc, const char ** args)
@@ -108,36 +135,35 @@ int main(int argc, const char ** args)
     auto usage = []()
     {
         std :: cout << "Usage: " << std :: endl;
-        std :: cout << "\t./demo.out rendezvous" << std :: endl;
-        std :: cout << "\t./demo.out peer [rendezvous ip] [setup time] [sources] [broadcasts] [broadcasts/second] [batch size] [release timeout] [peer id]" << std :: endl;
+        std :: cout << "\t./demo.out rendezvous [expected]" << std :: endl;
+        std :: cout << "\t./demo.out peer [rendezvous ip] [sources] [broadcasts] [broadcasts/second] [batch size] [release timeout] [peer id]" << std :: endl;
         return -1;
     };
 
-    if(argc < 2)
+    if(argc < 3)
         return usage();
 
     if(strcmp(args[1], "rendezvous") && strcmp(args[1], "peer"))
         return usage();
 
     if(!strcmp(args[1], "rendezvous"))
-        rendezvous();
+        rendezvous(std :: stoi(args[2]));
     else
     {
-        if(argc < 10)
+        if(argc < 9)
             return usage();
 
         try
         {
             class address :: ip directory(args[2]);
-            double setuptime = std :: stof(args[3]);
-            int sources = std :: stoi(args[4]);
-            int broadcasts = std :: stoi(args[5]);
-            double frequency = std :: stof(args[6]);
-            int batchsize = std :: stoi(args[7]);
-            double timeout = std :: stof(args[8]);
-            int id = std :: stoi(args[9]);
+            int sources = std :: stoi(args[3]);
+            int broadcasts = std :: stoi(args[4]);
+            double frequency = std :: stof(args[5]);
+            int batchsize = std :: stoi(args[6]);
+            double timeout = std :: stof(args[7]);
+            int id = std :: stoi(args[8]);
 
-            :: peer(id, directory, 1_s * setuptime, sources, broadcasts, 1_s / frequency, batchsize, 1_s * timeout);
+            :: peer(id, directory, sources, broadcasts, 1_s / frequency, batchsize, 1_s * timeout);
         }
         catch(...)
         {
